1be7b9753506873731b2cca262577f9122177983
Cosmo Kent <cosmolightfoot@gmail.com>
2019-02-04 17:57:00 -0800

    more work

---
+++ src/apply.js
@@ -0,0 +1,11 @@
+const formNode = document.getElementById('main-form');
+const nameNode = document.getElementById('name');
+const phoneNode = document.getElementById('phone')
+const comfortNode = document.getElementById('comfort');
+const comfortFieldNode = document.getElementById('comfort-field');
+
+comfortNode.addEventListener('change' function(){
+    if(comfortNode.value > 2) {
+        comfortFieldNode 
+    }
+});
