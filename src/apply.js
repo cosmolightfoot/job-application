0989300cf9249a104e9794d270dfc7d2d831e746
Cosmo Kent <cosmolightfoot@gmail.com>
2019-02-04 18:46:57 -0800

    range changes text field

--- src/apply.js
+++ src/apply.js
@@ -4,8 +4,29 @@ const phoneNode = document.getElementById('phone')
 const comfortNode = document.getElementById('comfort');
 const comfortFieldNode = document.getElementById('comfort-field');
 
-comfortNode.addEventListener('change' function(){
-    if(comfortNode.value > 2) {
-        comfortFieldNode 
+comfortNode.addEventListener('change', function(){
+    var messageSelector;
+    let select = comfortNode.value;
+    
+    var rangeValue = comfortNode.value;
+    console.log('comfort value', rangeValue);
+    switch (select) {
+        case '1': 
+            messageSelector = 'I have a strong aversion to being incinerated.';
+            break;
+        case '2': 
+            messageSelector = 'I am mildly averse to being burnt to a crisp.';
+            break;
+        case '3': 
+            messageSelector = 'I am neither pleased nor displeased by my calories being released as thermal energy.';
+            break;
+        case '4': 
+            messageSelector = 'I find the notion of becoming a smoking pile of ash acceptable.';
+            break;
+        case '5': 
+            messageSelector = 'SACRIFICE ME TO THE LORD OF LIGHT, FOR THE NIGHT IS DARK AND FULL OF TERRORS!!!';
+            break;
     }
+    comfortFieldNode.textContent = messageSelector;
 });
+
